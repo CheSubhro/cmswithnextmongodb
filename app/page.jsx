12d4db7942d34dbacd9572d07275f43862227a6e
CheSubhro <mitsubhra@gmail.com>
2024-02-29 12:01:18 +0530

    Change Page

--- app/page.jsx
+++ app/page.jsx
@@ -2,8 +2,8 @@ import CmsList from "@/components/CmsList";
 export default function Home() {
       return (
         <>
-        	{/* <CmsList/> */}
-			<div>Hello</div>
+        	<CmsList/>
+
         </>
       );
   }
